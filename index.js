ee1e8c15e0eb4f54c8e79bb562b3bea561588a50
Jose Torres <vidojam@gmail.com>
2023-03-31 17:54:40 -0400

    Ice Cream Parlor - final

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { client, iceCreamCollection } from'./mongoConnect.js';
 
-
+// Add ice cream
 const addIceCream = async () => {
   const addTypes = {
-    name: "Test",
+    name: "Single Scoop",
     price: 5, 
     description: "Two scoops of delicious ice cream" ,
   };
@@ -14,9 +14,57 @@ const addIceCream = async () => {
     console.log(addedIceCream);
     
   } catch (error) {
+
+};
+ //addIceCream()
+
+ // Edit ice cream
+ const editIceCream = async () => {
+  try {
+    await client.connect();
+    const updateIceCream = await iceCreamCollection.findOneAndUpdate()
+      { name: "Grape" };
+      { $set: { name: "Sundae", special; true }}
+      console.log(updateIceCream)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      await client.close()
+    } 
+  }
+};
+
+ // editIceCream()
+
+ // Delete ice cream
+ const deleteIceCream = async () => {
+  try {
+    await client.connect()
+    const itemDelete = await iceCreamCollection.deleteOne({ name: "Sundae" });
+    console.log(itemDelete);  
+  } catch (error) {
     console.log(error)
   } finally {
     await client.close()
   }
 };
- addIceCream()
+
+ // deleteIceCream()
+
+ // get all ice creams
+
+ const getAllIceCream = async () => {
+  
+  try {
+    await client.connect()
+    const allIceCreams = await iceCreamCollection.find().toArray()
+    console.log(allIceCreams)
+
+    } catch (error) {
+    console.log(error) 
+    } finally {
+      await client.close()
+    }
+};
+
+ // getAllIceCream();
